2c2c0a9392087b02a6ffed29a3902a2202436244
iDeZil <dezil2424@gmail.com>
2026-06-19 01:40:44 -0600

    Primer prueba de dashboard

---
+++ dashboard/app.R
@@ -0,0 +1,30 @@
+# Librerias 
+
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+library(readr)
+
+# Dashboard
+ui <- dashboardPage(title = "Proyecto Shiny", 
+                    skin = "green",
+  dashboardHeader(
+    title = "Proyecto"
+  ),
+  
+  dashboardSidebar(
+    
+  ),
+  
+  dashboardBody(
+    
+  )
+)
+  
+  
+# Servidor
+server = function(input, output){
+  
+}
+
+shinyApp(ui = ui, server = server)
