1abd02d9670d3fe0db9eda0e72c087aad1b16523
iDeZil <dezil2424@gmail.com>
2026-06-21 02:34:45 -0600

    Se sube correctamente el gráfico de barras que mostrará la proporción de estudiantes que desean continuar con sus estudios superiores

--- dashboard/app.R
+++ dashboard/app.R
@@ -8,7 +8,16 @@ library(readr)
 base <- read_delim("https://github.com/CharlieboyCR/Proyecto-de-XS-0129/blob/51bf4cb8a55c1f6955c89ff7b6e49d0271cc317a/student-por.csv",
                       delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
+grafico <- ggplot(base, aes(x = Medu, fill = higher)) + # Nota: hacer interactivo para cambiar entre padre y madre 
+  geom_bar(position = "fill")+
+  labs(x= "nivel eduvactivo de la madre", y= "Proporcion")+
+  scale_fill_manual(
+    values = c("yes" = "darkgreen",
+               "no" = "red"))
 
+grafico
+
+grafico
 # Dashboard
 ui <- dashboardPage(title = "Proyecto Shiny", 
                     skin = "yellow",
