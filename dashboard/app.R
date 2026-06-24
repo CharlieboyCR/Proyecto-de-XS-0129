174ea15716cb36e44541f2bb380a80615e7cde3d
iDeZil <dezil2424@gmail.com>
2026-06-24 00:22:10 -0600

    Se agrega la parte interactiva:
    1. Se usa radiobuttons para crear las opciones para analizar el gráfico con Madre o Padre.
    
    2. En el server se comenta el gráfico inicial para tenerlo de referencia.
    
    3. Con ayuda de if y else, se conecta a radiobuttons y cada vez que se escoja una de las opciones (madre o padre) el gráfico cambie.

--- dashboard/app.R
+++ dashboard/app.R
@@ -52,6 +52,12 @@ ui <- dashboardPage(title = "Proyecto Shiny",
         tabName = "grafico_3",
         h2("Aspiraciones de educación superior vs. educación parental"),
         p("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni ratione rem, odit dignissimos, laboriosam iure perferendis commodi reprehenderit, officiis ullam similique! Corporis, quaerat? Neque quae excepturi dolorem blanditiis magnam recusandae?"),
+        radioButtons(
+          inputId = "Selector_madre_padre", label = "Seleccionar familiar", inline = TRUE,
+          choices = c(
+            "Madre" = "Madre", "Padre" = "Padre"
+          )
+        ),
         plotOutput("grafico_3"),
         p("Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni ratione rem, odit dignissimos, laboriosam iure perferendis commodi reprehenderit, officiis ullam similique! Corporis, quaerat? Neque quae excepturi dolorem blanditiis magnam recusandae?")
       )
@@ -62,15 +68,38 @@ ui <- dashboardPage(title = "Proyecto Shiny",
   
 # Servidor
 server = function(input, output){
-  output$grafico_3 = renderPlot({
-    ggplot(base, aes(x = Medu, fill = higher)) + # Nota: hacer interactivo para cambiar entre padre y madre 
-      geom_bar(position = "fill")+
-      labs(x= "nivel eduvactivo de la madre", y= "Proporcion")+
-      scale_fill_manual(
-        values = c("yes" = "darkgreen",
-                   "no" = "red"))
+  
+  # NOTA: Este es el gráfico inicial
+  #output$grafico_3 = renderPlot({
+    #ggplot(base, aes(x = Medu, fill = higher)) +
+      #geom_bar(position = "fill")+
+      #labs(x= "nivel eduvactivo de la madre", y= "Proporcion")+
+      #scale_fill_manual(
+        #values = c("yes" = "darkgreen",
+                   #"no" = "red"))
     
-  })
-}
+    output$grafico_3 <- renderPlot({
+      
+      if(input$Selector_madre_padre == "Madre"){
+        ggplot(base, aes(x = Medu, fill = higher)) +
+          geom_bar(position = "fill")+
+          labs(x= "Nivel educativo de la madre")+
+          scale_fill_manual(values = c("yes" = "darkgreen",
+                     "no" = "red"))
+          
+        
+      } else {
+        ggplot(base, aes(x = Fedu, fill = higher)) +
+          geom_bar(position = "fill")+
+          labs(x= "Nivel educativo del padre")+
+          scale_fill_manual(values = c("yes" = "darkgreen",
+                     "no" = "red"))
+        
+      }
+      
+    })
+  }
+
 
 shinyApp(ui = ui, server = server)
+
