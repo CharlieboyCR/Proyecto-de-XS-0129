ce9088e21293d8883b882e76d306c1bfb68e02c6
CharlieboyCR <carloscalderon753@gmail.com>
2026-06-25 15:48:48 -0600

    Cambio de color de gráficos

--- dashboard/app.R
+++ dashboard/app.R
@@ -60,7 +60,7 @@ server = function(input, output){
     ggplot(base, aes(x = schoolsup, y = G3, fill = schoolsup)) +
       geom_boxplot(alpha = 0.7, outlier.shape = NA) + 
       geom_jitter(width = 0.2, alpha = 0.3, color = "black") + 
-      scale_fill_manual(values = c("red", "blue"), labels = c("No recibe apoyo", "Recibe apoyo")) +
+      scale_fill_manual(values = c("orange", "lightblue"), labels = c("No recibe apoyo", "Recibe apoyo")) +
       labs(
         title = "Distribución de Calificaciones Finales (G3)",
         subtitle = "Comparativa según recepción de apoyo educativo extra",
