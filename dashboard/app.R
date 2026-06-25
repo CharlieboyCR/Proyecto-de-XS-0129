20d48e8784fcc17515517509eaf7de8c7add88f1
DonQuijote29 <alberto.pinto@ucr.ac.cr>
2026-06-25 11:40:38 -0600

    Aqui estoy generando el gráfico básico. El gráfico es un diagrama de cajas que me permite comparar los resultados de quienes tienen internet versus quienes no. A partir de este gráfico quiero añadirle capas para explorar algunos temas más complejos.

--- dashboard/app.R
+++ dashboard/app.R
@@ -5,9 +5,9 @@ library(shinydashboard)
 library(tidyverse)
 library(readr)
 
-base <- read_delim("~/Proyecto-de-XS-0129/student-por_test.csv", 
+base <- read_delim("~/Proyecto-de-XS-0129/student-por.csv", 
                       delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
+View(base)
 # Dashboard
 ui <- dashboardPage(title = "Proyecto Shiny", 
                     skin = "yellow",
@@ -21,7 +21,8 @@ ui <- dashboardPage(title = "Proyecto Shiny",
         "Estudio y rendimiento académico"
       ),
       menuItem(
-        "Brecha digital en la educación"
+        "Brecha digital en la educación", 
+        tabName = "gráfico_2"
       ),
       menuItem(
         "Educación parental"
@@ -45,13 +46,31 @@ ui <- dashboardPage(title = "Proyecto Shiny",
   ),
   
   dashboardBody(
-    
+    tabItems(
+      tabItem(
+        tabName = "gráfico_2",
+        h2("Brecha digital en la educación"),
+        box(
+          width = 12,
+          plotOutput("boxplot")
+        )
+      )
+    )
   )
 )
   
   
 # Servidor
 server = function(input, output){
+  output$boxplot <- renderPlot ({
+    boxplot(
+      base$G3~base$internet,
+      main = "Brecha en la educación",
+      xlab = "Acceso a Internet"
+      ylab = "Valor",
+      col = "grey"
+    )
+  })
   
 }
 
