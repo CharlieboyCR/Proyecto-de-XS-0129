2ed3f364eb9537e8edf9d7d3993c8ac581ba2486
CharlieboyCR <carloscalderon753@gmail.com>
2026-06-25 15:43:58 -0600

    Se añadió el gráfico al dashboard

--- dashboard/app.R
+++ dashboard/app.R
@@ -4,10 +4,10 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(readr)
+library(DT)
 
-base <- read_delim("~/Proyecto-de-XS-0129/student-por_test.csv", 
+base <- read_delim("~/Proyecto-de-XS-0129/student-por.csv", 
                       delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
 # Dashboard
 ui <- dashboardPage(title = "Proyecto Shiny", 
                     skin = "yellow",
@@ -46,13 +46,29 @@ ui <- dashboardPage(title = "Proyecto Shiny",
   
   dashboardBody(
     
+    box(
+      plotOutput("plot_4", height = 400)
+    )
+    
   )
 )
   
   
 # Servidor
 server = function(input, output){
-  
+  output$plot_4 <- renderPlot({
+    ggplot(base, aes(x = schoolsup, y = G3, fill = schoolsup)) +
+      geom_boxplot(alpha = 0.7, outlier.shape = NA) + 
+      geom_jitter(width = 0.2, alpha = 0.3, color = "black") + 
+      scale_fill_manual(values = c("red", "blue"), labels = c("No recibe apoyo", "Recibe apoyo")) +
+      labs(
+        title = "Distribución de Calificaciones Finales (G3)",
+        subtitle = "Comparativa según recepción de apoyo educativo extra",
+        x = "Apoyo educativo extra",
+        y = "Calificación Final (G3)",
+        fill = "Grupo"
+      )
+  })
 }
 
 shinyApp(ui = ui, server = server)
@@ -60,8 +76,7 @@ shinyApp(ui = ui, server = server)
 
 
 ####
-library(readr)
-library(DT)
+
 student <- read_delim("student-por.csv", 
                           delim = ";", escape_double = FALSE, trim_ws = TRUE)
 View(student)
@@ -80,17 +95,7 @@ resumen_apoyo <- student %>%
 
 print(resumen_apoyo)
 
-ggplot(student, aes(x = schoolsup, y = G3, fill = schoolsup)) +
-  geom_boxplot(alpha = 0.7, outlier.shape = NA) + 
-  geom_jitter(width = 0.2, alpha = 0.3, color = "black") + 
-  scale_fill_manual(values = c("red", "blue"), labels = c("No recibe apoyo", "Recibe apoyo")) +
-  labs(
-    title = "Distribución de Calificaciones Finales (G3)",
-    subtitle = "Comparativa según recepción de apoyo educativo extra",
-    x = "Apoyo educativo extra",
-    y = "Calificación Final (G3)",
-    fill = "Grupo"
-  ) 
+
 
 
 
