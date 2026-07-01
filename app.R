4cf3f692fbf8a5dd6460b6c1795313cd57ed96da
albertojosepintoalpizar <alberto.pinto@ucr.ac.cr>
2026-07-01 12:24:30 -0600

    Commit final

--- app.R
+++ app.R
@@ -1,104 +1,4 @@
-#Cambios de Raque
-library(shiny)
-library(shinythemes)
-library(dplyr)
-library(ggplot2)
 
-
-base <- read_delim("student-por.csv", 
-                      delim = ";", escape_double = FALSE, trim_ws = TRUE)
-
-ui <- fluidPage(
-  theme = shinytheme("cosmo"), 
-  
-  titlePanel("Análisis de calificaciones de Portugués con respecto a el apoyo escolar y familiar"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      tags$h4("Filtros de Análisis"),
-      hr(),
-      
-      radioButtons("variable_apoyo", 
-                   label = "Seleccione el tipo de apoyo a analizar:",
-                   choices = c("Apoyo escolar" = "schoolsup",
-                               "Apoyo familiar" = "famsup"),
-                   selected = "schoolsup")
-      
-    
-      
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-       
-        tabPanel("Tabla Resumen", 
-                 br(),
-                 tags$h3("Métricas Descriptivas de G3"),
-                 p("A continuación se presentan la media, mediana, desviación estándar y conteo de estudiantes según el apoyo recibido:"),
-                 tableOutput("tabla_resumen")
-        ),
-        
-        
-        tabPanel("Gráficos de Distribución", 
-                 br(),
-                 plotOutput("boxplot_g3"),
-                 br(),
-                 plotOutput("histograma_g3")
-        )
-      )
-    )
-  )
-)
-
-
-server <- function(input, output) {
-  
-
-  output$tabla_resumen <- renderTable({
-    base %>%
-      group_by(Soporte = .data[[input$variable_apoyo]]) %>%
-      summarise(
-        `Total Estudiantes` = n(),
-        `Media de notas (G3)`   = round(mean(G3, na.rm = TRUE), 2),
-        `Mediana de notas (G3)` = median(G3, na.rm = TRUE),
-        `Desviación Estandar`  = round(sd(G3, na.rm = TRUE), 2)
-      )
-  })
-  
-  
-  output$boxplot_g3 <- renderPlot({
-    ggplot(base, aes(x = .data[[input$variable_apoyo]], y = G3, fill = .data[[input$variable_apoyo]])) +
-      geom_boxplot(alpha = 0.7, outlier.colour = "#891A1E", outlier.shape = 16) +
-      labs(
-        title = paste("Distribución de G3 según tipo de apoyo ", input$variable_apoyo),
-        x = paste("Recibe", input$variable_apoyo ),
-        y = "Calificación Final (G3)"
-      ) +
-      theme_minimal() +
-      scale_fill_brewer(palette = "Set1") +
-      theme(legend.position = "none", plot.title = element_text(face = "bold", size = 14))
-  })
-  
-  
-  output$histograma_g3 <- renderPlot({
-    ggplot(base, aes(x = G3, fill = .data[[input$variable_apoyo]])) +
-      geom_histogram(binwidth = 1, alpha = 0.6, position = "identity", color = "black") +
-      labs(
-        title = paste("Histograma de Frecuencias de G3 por tipo de apoyo", input$variable_apoyo),
-        x = "Calificación Final (G3)",
-        y = "Frecuencia Absoluta",
-        fill = input$variable_apoyo
-      ) +
-      theme_minimal() +
-      scale_fill_brewer(palette = "Set1") +
-      theme(plot.title = element_text(face = "bold", size = 14))
-  })
-}
-
-# Desplegar la aplicación
-shinyApp(ui = ui, server = server)
-
-#código original
 # Librerias
 library(shiny)
 library(shinydashboard)
@@ -165,6 +65,19 @@ ui <- dashboardPage(title = "Proyecto Shiny",
             selected = "Visual",
             inline = TRUE
           ),
+          conditionalPanel(
+            condition = "input.tipoGrafico == 'Formal'",
+          checkboxInput(
+            inputId = "Limpio",
+              label = "Eliminar los valores extremos"
+          ), 
+          sliderInput(
+            inputId = "alpha",
+              label = "Nivel de significancia",
+              min = 0.01,
+              max = 0.2,
+              value = 0.05,
+          )),
           plotOutput("grafico")
         )
       ),
@@ -234,14 +147,68 @@ ui <- dashboardPage(title = "Proyecto Shiny",
 server = function(input, output){
   output$grafico <- renderPlot ({
     if(input$tipoGrafico == "Visual") {
-    boxplot(
-      base$G3~base$internet,
-      main = "Brecha en la educación",
-      xlab = "Acceso a Internet",
-      ylab = "Valor",
-      col = "grey"
-    ) } else {
-      
+      if(input$Limpio){
+        Datos_Limpios2 <- base %>% 
+          mutate(Q1 = quantile(base$G3, 0.25) ,
+                 Q3 = quantile(base$G3, 0.75),
+                 IQR = Q3-Q1,
+                 lim_inf = Q1- IQR *1.5 , 
+                 lim_sup = Q3 + IQR *1.5) %>% 
+          filter(between(G3, lim_inf, lim_sup))
+        boxplot(
+          Datos_Limpios2$G3~Datos_Limpios2$internet,
+          main = "Brecha en la educación",
+          xlab = "Acceso a Internet",
+          ylab = "Valor",
+          col = "grey"
+        )
+      } else {
+        boxplot(
+        base$G3~base$internet,
+        main = "Brecha en la educación",
+        xlab = "Acceso a Internet",
+        ylab = "Valor",
+        col = "grey"
+      )}
+     } else {
+        if(input$Limpio){
+        Datos_Limpios2 <- base %>% 
+          mutate(Q1 = quantile(base$G3, 0.25) ,
+                 Q3 = quantile(base$G3, 0.75),
+                 IQR = Q3-Q1,
+                 lim_inf = Q1- IQR *1.5 , 
+                 lim_sup =Q3 + IQR *1.5) %>% 
+          filter(between(G3, lim_inf, lim_sup))
+        grupo1 <- Datos_Limpios2 %>% 
+          filter(internet == "yes")
+        grupo2 <- Datos_Limpios2 %>% 
+          filter(internet == "no")
+        
+        n1 <- nrow(grupo1)
+        n2 <- nrow(grupo2)
+        
+        x1 <- mean(grupo1$G3)
+        x2 <- mean(grupo2$G3)
+        
+        s1 <- sd(grupo1$G3)
+        s2 <- sd(grupo2$G3)
+        
+        sp <- sqrt(((n1-1)*s1^2 + (n2-1)*s2^2)/(n1+n2-2))
+        gl= length(base$G3)-2
+        
+        TObs <- (x1-x2)/(sp*sqrt(1/n1+1/n2))
+        TCrit <- qt(1-input$alpha/2, df = gl)
+        
+        z = seq(-4,4, length.out = 1000)
+        datosg2 <- data.frame(z = z, y = dt(z, df = gl))
+        ggplot(datosg2, aes(x=z, y=y))+
+          geom_line(size=1)+
+          geom_area(data = subset(datosg2, z <= -TCrit), fill = "red")+
+          geom_area(data = subset(datosg2, z >= TCrit), fill = "red")+
+          geom_point(aes(x=TObs, y = dt(TObs, df = gl)), size = 2)
+        
+        
+        } else {
       grupo1 <- base %>% 
         filter(internet == "yes")
       grupo2 <- base %>% 
@@ -260,7 +227,7 @@ server = function(input, output){
       gl= length(base$G3)-2
       
       TObs <- (x1-x2)/(sp*sqrt(1/n1+1/n2))
-      TCrit <- qt(1-0.05/2, df = gl)
+      TCrit <- qt(1-input$alpha/2, df = gl)
       
       z = seq(-4,4, length.out = 1000)
       datosg2 <- data.frame(z = z, y = dt(z, df = gl))
@@ -270,7 +237,7 @@ server = function(input, output){
         geom_area(data = subset(datosg2, z >= TCrit), fill = "red")+
         geom_point(aes(x=TObs, y = dt(TObs, df = gl)), size = 2)
       
-      
+        }
       }
   
   })
